ef2942c1342d3032ce308497c7cd24e3d6b2af44
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2026-01-09 15:50:37 -0400

    feat(rdb): add users schema and queries

---
+++ server/database/sql/queries/users.sql
@@ -0,0 +1,30 @@
+-- name: AddUser :exec
+INSERT INTO users (
+    access_token,
+    showed
+) VALUES (?, ?);
+
+-- name: GetAccessToken :one
+SELECT access_token FROM users
+WHERE id = 1;
+
+-- name: GetAPIKEY :one
+SELECT api_key FROM users
+WHERE
+    id = 1
+    AND showed = 0;
+
+-- name: AddUserID :exec
+UPDATE users
+SET user_id = ?
+WHERE id = 1 ;
+
+-- name: UpdateToken :exec
+UPDATE users
+SET access_token = ?
+WHERE id = 1 ;
+
+-- name: UpdateAPIKEY :exec
+UPDATE users
+SET api_key = ?
+WHERE id = 1 ;
