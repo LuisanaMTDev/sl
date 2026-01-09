e269597d713206d26e1da52a8b31bbe03b0a7660
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2026-01-09 19:28:39 -0400

    fix: correct project directory name

--- server/run_update_db_migrations.sh
+++ server/run_update_db_migrations.sh
@@ -2,6 +2,6 @@
 
 (
   source .env
-  trap 'cd $HOME/dev/spaced_learning/server' ERR
-  cd $HOME/dev/spaced_learning/server/database/sql/schemas && goose sqlite3 $DB_URL_DEV down-to 0 && goose sqlite3 $DB_URL_DEV up && cd $HOME/dev/spaced_learning/server
+  trap 'cd $HOME/dev/sl/server' ERR
+  cd $HOME/dev/sl/server/database/sql/schemas && goose sqlite3 $DB_URL_DEV down-to 0 && goose sqlite3 $DB_URL_DEV up && cd $HOME/dev/sl/server
 )
