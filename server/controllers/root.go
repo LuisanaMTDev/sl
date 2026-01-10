8d5feef551ba8822cfb310569ac047ee24588292
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2026-01-10 12:26:42 -0400

    fix: execution flow of root handler

--- server/controllers/root.go
+++ server/controllers/root.go
@@ -8,45 +8,44 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// TODO: Add auth for the CLI.
 func (sc *ServerConfig) Root(w http.ResponseWriter, r *http.Request) {
 	client := strings.ToUpper(r.Header.Get("SL-Client-Type"))
 
-	// TODO: Generate API key, send it to the user, and hash it and save it in the db.
-	if client == "SL-CLI" {
-		w.WriteHeader(http.StatusForbidden)
-		return
-	}
+	switch client {
+	case "SL-WEB-APP":
+		_, err := sc.DBQueries.GetAccessToken(r.Context())
+		if err != nil && err.Error() == "sql: no rows in result set" {
+			err = views.Index(
+				false,
+				sc.OAuthConfig.Scopes[0],
+				sc.OAuthConfig.ClientID,
+			).Render(r.Context(), w)
+			if err != nil {
+				log.Fatal().AnErr("error", err).Msg("ERROR: while sending main page")
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			return
+		} else if err != nil {
+			log.Fatal().AnErr("error", err).Msg("ERROR: while getting access token")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
-	_, err := sc.DBQueries.GetAccessToken(r.Context())
-	if err != nil && err.Error() == "sql: no rows in result set" {
+		log.Debug().Msg("SUCCESS: db has access token")
+
 		err = views.Index(
-			false,
-			sc.OAuthConfig.Scopes[0],
-			sc.OAuthConfig.ClientID,
+			true,
+			"",
+			"",
 		).Render(r.Context(), w)
 		if err != nil {
 			log.Fatal().AnErr("error", err).Msg("ERROR: while sending main page")
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		return
-	} else if err != nil {
-		log.Fatal().AnErr("error", err).Msg("ERROR: while getting access token")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	log.Debug().Msg("SUCCESS: db has access token")
-
-	err = views.Index(
-		true,
-		"",
-		"",
-	).Render(r.Context(), w)
-	if err != nil {
-		log.Fatal().AnErr("error", err).Msg("ERROR: while sending main page")
-		w.WriteHeader(http.StatusInternalServerError)
+	default:
+		w.WriteHeader(http.StatusForbidden)
 		return
 	}
 }
